dbf32f8b4c581bec2ed70f14977a686d1d6c62ab
Ferenc Koler <ferenckoler@outlook.com>
2024-10-26 22:31:09 +0200

    next/navigation

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,26 @@
+'use client';
+import {
+    useRouter,
+    useParams,
+    useSearchParams,
+    usePathname,
+} from 'next/navigation';
+
 const PropertyPage = () => {
-    return <div>PropertyPage</div>;
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { id } = useParams();
+
+    const name = searchParams.get('name');
+
+    return (
+        <div>
+            <button onClick={() => router.push()} className='bg-blue-500 p-2'>
+                Go Home {pathname}
+            </button>
+        </div>
+    );
 };
 
 export default PropertyPage;
