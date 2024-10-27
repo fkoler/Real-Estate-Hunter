5be28789bb6aab08b683d03c90abe3c9edee3fad
Ferenc Koler <ferenckoler@outlook.com>
2024-10-27 21:38:22 +0100

    Navbar basic structure

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,26 +1,6 @@
 'use client';
-import {
-    useRouter,
-    useParams,
-    useSearchParams,
-    usePathname,
-} from 'next/navigation';
-
 const PropertyPage = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const { id } = useParams();
-
-    const name = searchParams.get('name');
-
-    return (
-        <div>
-            <button onClick={() => router.push()} className='bg-blue-500 p-2'>
-                Go Home {pathname}
-            </button>
-        </div>
-    );
+    return <div>PropertyPage</div>;
 };
 
 export default PropertyPage;
